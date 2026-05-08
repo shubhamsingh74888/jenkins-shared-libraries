248761654ef8aee4c2eedff03ee0825e8457d9e5
shubham singh <shubhamsingh74888@gmail.com>
2026-05-08 22:05:58 +0530

    Refactor docker_push to avoid duplicate commands
    Removed redundant docker tag and push commands for 'flask-app'.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String project, String ImageTag, String dockerHubUser){
    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
                     sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+                    sh "docker tag ${project}:${ImageTag} ${dockerHubUser}/${project}:${ImageTag}"
+                    sh "docker push ${dockerHubUser}/${project}:${ImageTag}"
    }
-                    sh "docker tag flask-app:latest ${dockerHubUser}/flask-app:latest"
-                    sh "docker push ${dockerHubUser}/flask-app:latest"
 }
